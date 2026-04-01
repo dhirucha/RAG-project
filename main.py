8387774f23608bae547041e48da2ab60e1ab0673
Dheeraj_Chaubey <dheerajubecha@gmail.com>
2026-04-01 22:59:52 +0530

    Add initial project files and dependencies
    - Created notes.txt for project notes.
    - Added test.py as a placeholder for tests.
    - Implemented main.py to load environment variables and invoke the Mistral AI model.
    - Added requirements.txt to manage project dependencies including langchain, mistralai, and other libraries for document processing and web applications.

---
+++ main.py
@@ -0,0 +1,11 @@
+from dotenv import load_dotenv
+
+from langchain_mistralai import ChatMistralAI
+
+load_dotenv()
+
+model = ChatMistralAI( model = 'mistral-small-2506')
+
+result = model.invoke('hello')
+
+print(result.content)
