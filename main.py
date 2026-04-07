58881e232b9a53b06e555d59edc33d51d85728c3
Dheeraj_Chaubey <dheerajubecha@gmail.com>
2026-04-07 21:50:22 +0530

    Add initial database setup and document processing

--- main.py
+++ main.py
@@ -1,21 +1,16 @@
 from dotenv import load_dotenv
-from langchain_community .document_loaders import TextLoader
+from langchain_community .document_loaders import PyPDFLoader
 from langchain_mistralai import ChatMistralAI
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 
 load_dotenv()
 
-data = TextLoader("document_loaders/deep-learning.pdf")
+data = PyPDFLoader("document_loaders/deep-learning.pdf")
 docs = data.load()
 
 
-splitter = RecursiveCharacterTextSplitter(
-    chunk_size=1000,
-    chunk_overlap=200
-)
 
-chunks = splitter.split_documents(docs)
 
 template = ChatPromptTemplate.from_messages(
     [("system", "you are a AI that summarizes the text"),
@@ -24,8 +19,3 @@ template = ChatPromptTemplate.from_messages(
 
 model = ChatMistralAI( model = 'mistral-small-2506')
 
-prompt = template.format_messages(data = docs[0].page_content)
-
-result = model.invoke(prompt)
-
-print(result.content)
